d8d53bcabee550e4f7a29a7dfe9942737f093939
Ricardo Vera <ricardo@uag.com>
2015-01-19 20:10:29 -0600

    Initial Commit

---
+++ Lab04/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab04
+//
+//  Created by Ricardo Vera on 19/01/15.
+//  Copyright (c) 2015 Comercomp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
