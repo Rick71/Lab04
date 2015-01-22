917cba192651683465701c10f42a86ad5968b53c
Ricardo Vera <ricardo@uag.com>
2015-01-21 22:39:48 -0600

    continuación laboratorio 04, creación de zaga hacia Score

---
+++ Lab04/Scores.h
@@ -0,0 +1,13 @@
+//
+//  Scores.h
+//  Lab04
+//
+//  Created by Ricardo Vera on 21/01/15.
+//  Copyright (c) 2015 Comercomp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Scores : UIViewController
+
+@end
